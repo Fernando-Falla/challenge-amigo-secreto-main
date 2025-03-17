768fd4d78b99ed72602641fc46a5d7d6ffff8f7d
Fernando Falla <livesurfer01@gmail.com>
2025-03-17 11:46:33 -0500

    Creo Js Semifuncional

--- app.js
+++ app.js
@@ -1,2 +1,38 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 //Arrancamos hoy 16 de marzo, si. un poco tarde
+//esta tarea me ha llevado toda la tarde del domingo
+
+let listaAmigos=[];
+let numeroMaximo= 0;
+
+function agregarAmigo() {
+    // Obtener el valor del input amigo
+    let nombreIngresado = document.getElementById("amigo").value;
+   
+   
+    if (nombreIngresado.trim() !== "") {
+        alert("Nombre no valido");
+         } else {
+         // Agregar a la lista (array)
+    listaAmigos.push(nombreIngresado);
+        }
+    // Mostrar en la lista HTML
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML += "<li>" + nombreIngresado + "</li>";
+    
+    // Limpiar la caja (el input)
+    document.getElementById("amigo").value = "";
+
+    numeroMaximo++
+
+    console.log(listaAmigos);
+   
+    console.log (numeroMaximo);
+
+    if (numeroMaximo ==5) {
+        alert("Máximo 5 Amigos, pulse Sortear Amigo");
+    } else {
+        return;
+    }  
+   
+}
